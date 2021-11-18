f4eabc68e3ac185e974ab674816e389b5f43b35c
KtKeaton <ktr01@protonmail.com>
2021-11-19 00:18:08 +0800

    基本檢索、聚合函數、萬用字元

--- sql_company.sql
+++ sql_company.sql
@@ -67,3 +67,60 @@ WHERE `branch_id` = 2;
 UPDATE `branch`
 SET `manager_id` = 208
 WHERE `branch_id` = 3;
+
+INSERT INTO `client` VALUES(400, "阿狗", "0900123335");
+INSERT INTO `client` VALUES(401, "阿貓", "0900123123");
+INSERT INTO `client` VALUES(402, "旺來", "0900123123");
+INSERT INTO `client` VALUES(403, "露西", "0900123123");
+INSERT INTO `client` VALUES(404, "艾瑞克", "0900123123");
+
+-- 演練
+-- 取得所有員工資料
+SELECT * FROM `employee`;
+
+-- 取得所有客戶資料（）
+SELECT * FROM `client`;
+
+-- 按薪水低到高抓員工
+SELECT * FROM `employee` order by `salary` DESC ;
+
+-- 取得薪水前三高的員工
+SELECT * FROM `employee` order by `salary` DESC LIMIT 3;
+
+-- 取得所有員工名字
+SELECT `name` FROM `employee`;
+
+-- 聚合函數
+-- 取得員工人數alter
+SELECT COUNT(*) FROM `employee`;
+
+-- 取得所有出生於 1970-01-01 之後的女性員工人數
+SELECT COUNT(*) FROM `employee` WHERE `birth_date` > '1970-01-01' AND `sex` = 'F';
+
+-- 取得所有員工的平均薪水
+SELECT AVG(`salary`) FROM `employee`;
+
+-- 取得所有員工的薪水總和
+SELECT SUM(`salary`) FROM `employee`;
+
+-- 取得薪水最高的員工 
+SELECT MAX(`salary`) FROM `employee`;
+-- 原本這樣寫
+SELECT `salary` FROM `employee` ORDER BY `salary` DESC LIMIT 1;
+
+-- 取得薪水最低的員工 
+SELECT MIN(`salary`) FROM `employee`;
+-- 原本這樣寫
+SELECT `salary` FROM `employee` ORDER BY `salary` LIMIT 1;
+
+-- 萬用字元 
+ -- % 代表多字元
+ -- _ 代表單一字元
+-- 取得電話號碼尾數是335的客戶
+SELECT * FROM `client` WHERE `phone` LIKE '%335';
+
+-- 取得姓艾的客戶
+SELECT * FROM `client` WHERE `client_name` LIKE '艾%';
+
+-- 取得生日在12月的員工
+SELECT * FROM `employee` WHERE `birth_date` LIKE '_____12%';
